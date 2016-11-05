5ae83e6a523719fc54de175fde495ab73045c691
Miguel Hernandez <phyzix5761@gmail.com>
2016-11-05 13:33:30 -0400

    Added Catch Update

--- src/main/java/com/lodenrogue/fishingtournament/controller/CatchController.java
+++ src/main/java/com/lodenrogue/fishingtournament/controller/CatchController.java
@@ -1,12 +1,18 @@
 package com.lodenrogue.fishingtournament.controller;
 
-import com.lodenrogue.fishingtournament.model.Catch;
-import com.lodenrogue.fishingtournament.service.CatchFacade;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lodenrogue.fishingtournament.model.Catch;
+import com.lodenrogue.fishingtournament.service.CatchFacade;
 
 @CrossOrigin
 @RestController
@@ -29,6 +35,19 @@ public class CatchController {
 	public ResponseEntity<Object> getAllUnreviewedCatches() {
 		List<Catch> catches = new CatchFacade().findUnreviewed();
 		return new ResponseEntity<Object>(catches, HttpStatus.OK);
+	}
+
+	@RequestMapping(path = "/catches/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Object> updateCatch(@PathVariable long id, @RequestBody Catch catchEntry) {
+		
+		// Check that catch exists
+		Catch existing = new CatchFacade().find(id);
+		if(existing == null){
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+		}
+		
+		catchEntry = new CatchFacade().update(catchEntry);
+		return new ResponseEntity<Object>(catchEntry, HttpStatus.OK);
 	}
 
 }
